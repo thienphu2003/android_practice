3ee6c189157d69a9e856fd7b754d0747694909a2
volamthienphu <phuvo@hitek.com.vn>
2024-05-20 16:49:01 +0700

    practice array

---
+++ Project1/app/src/main/java/com/thienphu/firstproject/Arrays.kt
@@ -0,0 +1,10 @@
+package com.thienphu.firstproject
+
+fun main(args: Array<String>) {
+    var age : Array<Int> = arrayOf(1,2,3)
+    println("First element :"+ age[0])
+    println("Second element :"+ age.get(1))
+    println("Third element :"+ age[2])
+
+    
+}
