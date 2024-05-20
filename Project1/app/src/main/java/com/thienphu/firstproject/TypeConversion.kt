87e2c73b3837d9b7bfd0c1c83d38324bae240219
volamthienphu <phuvo@hitek.com.vn>
2024-05-20 16:35:25 +0700

    practice type conversion

---
+++ Project1/app/src/main/java/com/thienphu/firstproject/TypeConversion.kt
@@ -0,0 +1,18 @@
+package com.thienphu.firstproject
+
+fun main(args: Array<String>) {
+//    var x :Byte = 127
+//    var y : Int = x.toInt()
+//    var z :Double = y.toDouble()
+//
+//    println(x)
+//    println(y)
+//    println(z)
+
+    var number : Double = 132.32
+    var x : Int = number.toInt()
+    var z : Byte = x.toByte()
+    println(number)
+    println(x)
+    println(z)
+}
