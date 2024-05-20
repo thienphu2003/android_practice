11bdbbd9718dfc28203339441754ff17887704c6
volamthienphu <phuvo@hitek.com.vn>
2024-05-20 16:28:21 +0700

    practice data types

---
+++ Project1/app/src/main/java/com/thienphu/firstproject/DataTypes.kt
@@ -0,0 +1,23 @@
+package com.thienphu.firstproject
+
+fun main(args: Array<String>) {
+    var a : Boolean = true
+    var b : Char = 'R'
+    var c : Byte = 12
+    var d : Short = -356
+    var e :Int = 43543
+    var f : Long = 34454422L
+    var g : Float = 3444123F
+    var h : Double = 322.0
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+
+}
