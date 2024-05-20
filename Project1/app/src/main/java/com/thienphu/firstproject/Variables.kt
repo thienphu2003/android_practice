30cbc3aed64d38d75f3001f0c00d5f2c79dcc2b4
volamthienphu <phuvo@hitek.com.vn>
2024-05-20 16:22:41 +0700

    practice variables

---
+++ Project1/app/src/main/java/com/thienphu/firstproject/Variables.kt
@@ -0,0 +1,12 @@
+package com.thienphu.firstproject
+
+fun main(args: Array<String>) {
+    var a = 10
+    var b = 20
+    var c = a + b
+    println(c)
+
+    var age = 10
+    age =20
+    println(age)
+}
